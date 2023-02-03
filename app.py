c7ed02149f0f5e1518e4a63aaac018cf8c73335e
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-02-04 00:07:09 +0530

    working with riseup

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, render_template
+from flask_mail import Mail,Message
 from manga_py.util import run_util
+import smtplib, ssl
 import subprocess,os
 import uuid
 
 app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='mail.riseup.net',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = 'scrclab@riseup.net',
+    MAIL_PASSWORD = ''
+)
+mail = Mail(app)
 
 @app.route("/",methods=["GET"])
 def main():
     EMAIL = "pacchu@kindle.com"
-    #URL = 'https://www.webtoons.com/en/challenge/bright-futures/list?title_no=824476'
-    URL = 'https://www.mangaread.org/manga/vagabond/'
+    URL = 'https://www.webtoons.com/en/fantasy/crimson-heart/list?title_no=4898'
     START_VOLUME = 0
     GET_VOLUME = 1
-    gen_name = str(uuid())
+
+    gen_name = str(uuid.uuid1())
     test_args = {
         'url': URL, 
         'name': gen_name, 
@@ -57,9 +67,16 @@ def main():
 
     x = run_util(test_args)
     files = os.listdir(f'static/{gen_name}/')
-    #subprocess.run(["kcc-c2e","-m","-p","KPW","-f","EPUB","static/vagabond/vol_001.cbz"])
-    print("done")
+    subprocess.run(["kcc-c2e","-m","-p","KPW","-f","MOBI",f"static/{gen_name}/{files[0]}"])
+    msg = Message(files[0],
+                  sender="scrclab@riseup.net",
+                  recipients=["pacchu@kindle.com"]
+                )
+    with app.open_resource(f"static/{gen_name}/{files[0]}".replace(".cbz",".mobi")) as fp:
+        msg.attach(f"{files[0]}".replace(".cbz",".mobi"), 'application/x-mobipocket-ebook', fp.read())
+    mail.send(msg)
+    return "0"
 
 if(__name__=="__main__"):
-    #app.run("0.0.0.0",8090)
+    app.run("0.0.0.0",8090)
     main()

bc438058256178c1e035b11506e688e7239a68fe
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-02-04 16:30:45 +0530

    threaded

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from flask_mail import Mail,Message
 from manga_py.util import run_util
 import smtplib, ssl
+from threading import Thread
 import subprocess,os
 import uuid
 
@@ -10,27 +11,22 @@ app.config.update(
     MAIL_SERVER='mail.riseup.net',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'scrclab@riseup.net',
+    MAIL_USERNAME = 'panifically@riseup.net',
     MAIL_PASSWORD = ''
 )
+
+ERRCOUNT = 3
+
 mail = Mail(app)
+threaded_queue = [] #this is so bad
 
-@app.route("/",methods=["GET","POST"])
-def main():
-    if request.method == 'GET':
-        return render_template("index.html")
+#TODO do a threaded queue to serve one manga request at once [Celery?]
+def fetch_and_deliver(__request__,URL,EMAIL,START_VOLUME,GET_VOLUME):
+    with app.test_request_context():
+        from flask import request
+        request = __request__
 
-    if request.method == 'POST':
-        print(request.form)
-        URL = request.form.get('manga-link')
-        START_VOLUME = abs(int(request.form.get('chapter')))
-        EMAIL = request.form.get('email')
-
-        START_VOLUME = 0
-        GET_VOLUME = 1
-
-        gen_name = str(uuid.uuid1())
-        
+        gen_name = str(uuid.uuid1())   
         test_args = {
             'url': URL, 
             'name': gen_name, 
@@ -72,18 +68,49 @@ def main():
             'debug_version': None, 
             'quiet': False
         }
-
-        x = run_util(test_args)
+        for attempt in range(ERRCOUNT):
+            try:
+                run_util(test_args)
+                break
+            except:
+                if(attempt == ERRCOUNT-1):
+                    return "Failed"
         files = os.listdir(f'static/{gen_name}/')
-        subprocess.run(["kcc-c2e","-m","-p","KPW","-f","MOBI",f"static/{gen_name}/{files[0]}"])
+        is_webtoon = "-m"
+        if("webtoon" in URL):
+            is_webtoon = "-w"
+        subprocess.run(["kcc-c2e",is_webtoon,"-p","KPW","-f","EPUB",f"static/{gen_name}/{files[0]}"])
         msg = Message(files[0],
-                    sender="scrclab@riseup.net",
+                    body=f"Your requested manga from {URL} is delivered\n Cheers, pacchu",
+                    sender=app.config['MAIL_USERNAME'],
                     recipients=[EMAIL]
                     )
-        with app.open_resource(f"static/{gen_name}/{files[0]}".replace(".cbz",".mobi")) as fp:
-            msg.attach(f"{files[0]}".replace(".cbz",".mobi"), 'application/x-mobipocket-ebook', fp.read())
+        with app.open_resource(f"static/{gen_name}/{files[0]}".replace(".cbz",".epub")) as fp:
+            msg.attach(f"{files[0]}".replace(".cbz",".epub"), 'application/epub+zip', fp.read())
         mail.send(msg)
-        return "Sending!"
+        print(f"Sent mail to mailto://{EMAIL}")
+        threaded_queue.pop()
+        return "Ok"
+
+@app.route("/",methods=["GET","POST"])
+def main():
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    if request.method == 'POST':
+        URL = request.form.get('manga-link')
+        START_VOLUME = int(request.form.get('chapter'))
+        EMAIL = request.form.get('email')
+
+        if(START_VOLUME < 0):
+            return render_template("index.html",err="Use proper numbers for chapter count. Can't afford a time machine") 
+        if("@" not in EMAIL or len(EMAIL) < 1):
+            return render_template("index.html",err="null@null.null not found? Do you have a kindle?") 
+        GET_VOLUME = 1
+        do_in_thread = Thread(target=fetch_and_deliver,args=(request,URL,EMAIL,START_VOLUME,GET_VOLUME))
+        do_in_thread.start()
+        threaded_queue.append(do_in_thread)
+        return render_template("index.html",saysomething=f"Your request has been recorded and will be delivered\nCurrently {len(threaded_queue)} pending") 
 
 if(__name__=="__main__"):
     app.run("0.0.0.0",8090)
